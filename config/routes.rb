3bfe8fc6e62bdc330c63cdcd32dbca220186f0fc
divaweb <lanittad@hotmail.com>
2018-08-30 08:39:19 -0600

    CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :wikis
   devise_for :users
-  resources 'welcome'
-  root to: 'welcome#index'
+  get 'welcome/index'
+  root  'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
