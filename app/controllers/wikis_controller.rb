769c9b0fbbe3bc27694b47a4015a345585d59003
divaweb <lanittad@hotmail.com>
2018-09-04 10:08:42 -0600

    corrections

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,9 +1,9 @@
 class WikisController < ApplicationController
-before_action :authenticate_user!, except: :index
+#before_action :authenticate_user!, except: :index
   #skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @wikis = Wiki.all
-    authorize @wikis
+    #authorize @wikis
   end
 
   def show
