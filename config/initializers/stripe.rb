534e612a7bcebbaf6b6bcc1852e9c1597f12d7ae
divaweb <lanittad@hotmail.com>
2018-09-04 07:24:29 -0600

    upgrade

---
+++ config/initializers/stripe.rb
@@ -0,0 +1,7 @@
+Rail.configuration.stripe = {
+  pulishable_key: ENV['STRIPE_PUBLISHABLE_KEY'],
+  secret_key: ENV['STRIPE_SECRET_KEY']
+
+}
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
