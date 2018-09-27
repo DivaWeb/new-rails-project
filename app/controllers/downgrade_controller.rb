7fb17899efd06b89f593d37944fdc73c3ec8c6ac
Divaweb <lanittad@hotmail.com>
2018-09-27 08:24:37 -0700

    upgrade

--- app/controllers/downgrade_controller.rb
+++ app/controllers/downgrade_controller.rb
@@ -2,6 +2,6 @@ class DowngradeController < ApplicationController
   def update
     current_user.update(role: "standard")
     flash[:notice] = "Your account has been dongraded to a standard account. Your private wikis are now public"
-    redicrect_to current_user
+    redicrect_to root_path
   end
 end
