2a91274ee2b192dcf027f8be95973c360bc99fe5
Divaweb <lanittad@hotmail.com>
2018-09-28 09:59:47 -0700

    correction

--- app/controllers/downgrades_controller.rb
+++ app/controllers/downgrades_controller.rb
@@ -1,7 +1,7 @@
-class DowngradeController < ApplicationController
+class DowngradesController < ApplicationController
   def update
     current_user.update(role: "standard")
     flash[:notice] = "Your account has been dongraded to a standard account. Your private wikis are now public"
-    redicrect_to root_path
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :wikis
-  resources :downgrade
+  resources :downgrades
   resources :charges, only: [:new, :create]
   devise_for :users
   get 'welcome/index'
