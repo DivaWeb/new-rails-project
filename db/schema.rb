6313e7bc1aabeb1c4cc088cee856768e86999959
Divaweb <lanittad@hotmail.com>
2018-10-21 15:35:54 -0700

    wikis correction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_11_140538) do
+ActiveRecord::Schema.define(version: 2018_10_21_223055) do
 
   create_table "collaborators", force: :cascade do |t|
     t.integer "wiki_id"
